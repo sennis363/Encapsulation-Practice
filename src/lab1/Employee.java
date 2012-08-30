eb7a1e405ee1195f055ed7744ddb86b1f15399c9
sennis <sennis@L11115038625.wctc.msft>
2012-08-30 13:52:15 -0500

    First commit.

---
+++ src/lab1/Employee.java
@@ -0,0 +1,76 @@
+package lab1;
+
+import java.util.Date;
+
+/**
+ * In this lab your challenge is to fix the code in both classes to use
+ * proper encapsulation and the four other best practices as explained by 
+ * your instructor.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.01
+ */
+public class Employee {
+    String firstName;
+    String lastName;
+    public String ssn;
+    public Date birthDate;
+    boolean metWithHr;
+    boolean metDeptStaff;
+    boolean reviewedDeptPolicies;
+    boolean movedIn;
+    String cubeId;
+
+    public Employee() {
+
+    }
+
+    // Assume this must be performed first
+    public void meetWithHrForBenefitAndSalryInfo() {
+        metWithHr = true;
+    }
+
+    // Assume this is must be performed second
+    public void meetDepartmentStaff() {
+        if(metWithHr) {
+            metDeptStaff = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot meet with "
+                    + "department staff until you have met with HR.");
+        }
+    }
+
+    // Assume this must be performed third
+    public void reviewDeptPolicies() {
+        if(metWithHr && metDeptStaff) {
+            reviewedDeptPolicies = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot review "
+                    + " department policies until you have first met with HR "
+                    + "and then with department staff.");
+        }
+    }
+
+    // Assume this must be performed 4th
+    public void moveIntoCubicle(String cubeId) {
+        if(metWithHr && metDeptStaff && reviewedDeptPolicies) {
+            this.cubeId = cubeId;
+            this.movedIn = true;
+        } else {
+            throw new IllegalStateException("Sorry, you cannot move in to a "
+                    + "cubicle until you have first met with HR "
+                    + "and then with department staff, and then reviewed"
+                    + "department policies.");
+        }
+
+    }
+
+    public String getStatus() {
+        if(metWithHr && metDeptStaff
+           && reviewedDeptPolicies && movedIn) {
+            return "Orientation is complete";
+        } else {
+            return "Orientation in progress...";
+        }
+    }
+}
