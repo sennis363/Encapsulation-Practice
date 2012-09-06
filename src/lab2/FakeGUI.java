4e96898e8a194fe075ba1c881fadbdc79f4f37c9
Michael <Michael@Sean-PC>
2012-09-05 19:04:27 -0500

    Completed all methods. All methods display the data entered back on the screen.

--- src/lab2/FakeGUI.java
+++ src/lab2/FakeGUI.java
@@ -9,5 +9,44 @@ package lab2;
  * @author Michael
  */
 public class FakeGUI {
+    private String move;
+    private String attack;
+    private String defend;
+
+    public String getMove() {
+        return move;
+    }
+
+    public void setMove(String move) {
+        move = "2 spaces foward";
+        this.move = move;
+    }
+
+    public String getAttack() {
+        return attack;
+    }
+
+    public void setAttack(String attack) {
+        if(move == "2 spaces foward") {
+            attack = "Ninja Chop";
+           this.attack = attack;
+        } else {
+            throw new IllegalStateException("Sorry, you must move first.");
+        }
+    }
+
+    public String getDefend() {
+        return defend;
+    }
+
+    public void setDefend(String defend) {
+        if(attack == "Ninja Chop") {
+            defend = "Turtle Shield";
+           this.defend = defend;
+        } else {
+            throw new IllegalStateException("Sorry, you must attack before "
+                    + "you can defend.");
+        }
+    }
     
 }
--- src/lab2/Startup.java
+++ src/lab2/Startup.java
@@ -12,6 +12,7 @@ public class Startup {
     public static void main(String[] args) {
         Game game = new Game();
         GameWorld world = new GameWorld();
+        FakeGUI gui = new FakeGUI();
         
         game.setNumPlayers(1);
         game.setDifficulty("easy");
@@ -20,6 +21,10 @@ public class Startup {
         world.setMapName("Dark Star");
         world.setWeather("Wormhole Travel");
         world.setCheats("On");
+        
+        gui.setMove("2 spaces foward");
+        gui.setAttack("Ninja Chop");
+        gui.setDefend("Turtle Shield");
         
         System.out.println("GAME SETTINGS");
         System.out.println("Players : " + game.getNumPlayers());
@@ -30,5 +35,10 @@ public class Startup {
         System.out.println("Map Name : " + world.getMapName());
         System.out.println("Weather : " + world.getWeather());
         System.out.println("Cheats : " + world.getCheats());
+        System.out.println("");
+        System.out.println("GUI COMMANDS");
+        System.out.println("Movement : " + gui.getMove());
+        System.out.println("Attack Ability: " + gui.getAttack());
+        System.out.println("Defending Ability : " + gui.getDefend());
     }
 }
